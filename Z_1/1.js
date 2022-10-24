9f36b0bfd580adb5a55658607ce62f95b3584cb8
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-10-24 18:00:10 +0700

    Домашняя работа 3

---
+++ Z_1/1.js
@@ -0,0 +1,5 @@
+const number=parseInt(prompt(("Введите температуру в грудусах Цельсия ")));
+const result=(9/5)*number+32;
+alert("Температура градусов по Фаренгейту " + result.toFixed(2));
+
+
---
+++ Z_2/2.js
@@ -0,0 +1,5 @@
+function showName(name) {
+    alert((`Привет, у тебя замечательное имя - ${name}`));
+}
+const username = prompt(("Введите ваше имя"));
+showName(username);
